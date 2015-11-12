6057ffc4d53630d9795d6eff77d3885e2a27c012
Arnold <arnoldsander@gmail.com>
2015-11-12 00:32:40 -0500

    small change to requires

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,7 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
+//= require employees
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
